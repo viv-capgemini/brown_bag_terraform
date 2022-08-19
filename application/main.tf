6b2871a0e368a3b8810f198bed9c723364e7d097
Vivienne Ansah <voa2000@hotmail.com>
2022-08-19 11:19:13 +0100

    Updated helm data sources

--- application/main.tf
+++ application/main.tf
@@ -1,8 +1,3 @@
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-# }
-
 provider "aws" {
   region = var.region
 }
@@ -36,9 +31,5 @@ data "terraform_remote_state" "network" {
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "18.26.6"
-
-  cluster_name    = data.terraform_remote_state.cluster.outputs.cluster_name
-
-  vpc_id     = data.terraform_remote_state.network.outputs.vpc_id
-  subnet_ids = data.terraform_remote_state.network.outputs.private_subnets
-    }
+  cloudwatch_log_group_name = "brown_bag_logs"
+}
