a8e4d6929cee08c3f67da81f8e6708b6d59d42a4
Vivienne Ansah <voa2000@hotmail.com>
2022-08-19 10:30:53 +0100

    update data source refs

--- application/kubernetes.tf
+++ application/kubernetes.tf
@@ -4,7 +4,7 @@ data "aws_eks_cluster_auth" "cluster" {
 
 provider "kubernetes" {
   host                   = data.terraform_remote_state.cluster.outputs.cluster_endpoint
-  cluster_ca_certificate = base64decode(data.terraform_remote_state.cluster.certificate_authority.0.data)
+  cluster_ca_certificate = base64decode(dmodule.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
     args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.cluster.outputs.cluster_name]
