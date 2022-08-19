743ca1c3841d05c1b689e90ef3d078940fda6a34
Vivienne Ansah <voa2000@hotmail.com>
2022-08-19 10:51:29 +0100

    Added helm data sources

--- application/helm_release.tf
+++ application/helm_release.tf
@@ -1,10 +1,10 @@
 provider "helm" {
   kubernetes {
-    host                   = data.terraform_remote_state.cluster.outputs.endpoint
-    cluster_ca_certificate = base64decode(data.terraform_remote_state.cluster.outputs.certificate_authority.0.data)
+    host                   = data.terraform_remote_state.cluster.outputs.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.certificate_authority.0.data)
     exec {
       api_version = "client.authentication.k8s.io/v1alpha1"
-      args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.cluster.outputs.cluster.name]
+      args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.cluster.outputs.cluster_name]
       command     = "aws"
     }
   }
