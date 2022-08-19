ddb2ed3c564c5545105f81a7b71dfd830f219441
Vivienne Ansah <voa2000@hotmail.com>
2022-08-19 08:45:29 +0100

    removed duplicated kubernates

--- application/main.tf
+++ application/main.tf
@@ -1,7 +1,7 @@
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-}
+# provider "kubernetes" {
+#   host                   = module.eks.cluster_endpoint
+#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+# }
 
 provider "aws" {
   region = var.region
