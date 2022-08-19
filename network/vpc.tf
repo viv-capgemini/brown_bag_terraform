a583faba335b36558676683b1a4e6a630879cfe7
viv-capgemini <100429286+viv-capgemini@users.noreply.github.com>
2022-08-19 12:24:29 +0100

    Update vpc.tf

--- network/vpc.tf
+++ network/vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.2"
 
-  name = "brown_bag-vpc"
+  name = "demo_brown_bag-vpc"
 
   cidr = var.vpc_cidr
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
