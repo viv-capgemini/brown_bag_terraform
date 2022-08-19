206b4a045ba186e64dddd01ef51cfe33e4f1aa1c
Vivienne Ansah <voa2000@hotmail.com>
2022-08-19 10:40:07 +0100

    Added data sources

--- application/main.tf
+++ application/main.tf
@@ -20,3 +20,25 @@ data "terraform_remote_state" "cluster" {
     }
   }
 }
+
+data "terraform_remote_state" "network" {
+  backend = "remote"
+
+  config = {
+
+    organization = "brown_bag_demo"
+    workspaces = {
+          name = "network"
+    }
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.26.6"
+
+  cluster_name    = data.terraform_remote_state.cluster.outputs.cluster_name
+
+  vpc_id     = data.terraform_remote_state.network.outputs.vpc_id
+  subnet_ids = data.terraform_remote_state.network.outputs.private_subnets
+    }
